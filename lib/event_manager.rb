725d84466edfeea9519618afa8c8d3dabeb0c741
datrolld20 <datrolld20@gmail.com>
2020-10-07 18:33:23 -0400

    Initial commit

---
+++ lib/event_manager.rb
@@ -0,0 +1,19 @@
+require 'csv'
+
+root = '..'
+attendees = "#{root}/event_attendees.csv"
+contents = CSV.open(attendees, headers: true, header_converters: :symbol)
+
+# Returns zipcode set to 5 digits, with 0s at beginning if too short, including if nil
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0').slice(0, 5)
+end
+
+puts 'Event Manager initialized.'
+
+contents.each do |row|
+  name     = row[:first_name]
+  zipcode  = clean_zipcode(row[:zipcode])
+
+  puts "#{name}, #{zipcode}"
+end

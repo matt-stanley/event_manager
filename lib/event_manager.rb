4a71a90069eed178399c9c4cfc238a6ea2b197b7
datrolld20 <datrolld20@gmail.com>
2020-10-08 02:38:12 -0400

    Add JSON parsing for Google civic API

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,23 @@
 require 'csv'
-require 'google/apis/civicinfo_v2'
+require 'open-uri'
+require 'json'
 
-root = '..'
-attendees = "#{root}/event_attendees.csv"
+attendees = '../event_attendees.csv'
 contents = CSV.open(attendees, headers: true, header_converters: :symbol)
+
+# Example API call
+# https://www.googleapis.com/civicinfo/v2/representatives?address=80203&levels=country&roles=legislatorUpperBody&roles=legislatorLowerBody&key=AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw
 
 # Returns zipcode set to 5 digits, with 0s at beginning if too short, including if nil
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0').slice(0, 5)
+end
+
+def make_api_call(zipcode)
+  base_url = 'https://www.googleapis.com/civicinfo/v2/representatives?'
+  key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+
+  URI.open("#{base_url}address=#{zipcode}&levels=country&roles=legislatorUpperBody&roles=legislatorLowerBody&key=#{key}").read
 end
 
 puts 'Event Manager initialized.'
@@ -15,6 +25,8 @@ puts 'Event Manager initialized.'
 contents.each do |row|
   name     = row[:first_name]
   zipcode  = clean_zipcode(row[:zipcode])
+
+  rep_info = JSON.parse(make_api_call(zipcode))
 
   puts "#{name}, #{zipcode}"
 end
